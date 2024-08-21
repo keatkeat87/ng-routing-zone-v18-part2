90c2954c0a99f01aa4e71790b531a255553a0358
Derrick Yam <keatkeat87@live.com>
2024-08-21 13:16:19 +0800

    ok

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { ApplicationRef, Component, inject, Injector, TemplateRef, viewChild, ViewContainerRef } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+
+  private readonly templateRef = viewChild.required('template', { read: TemplateRef });
+  private readonly viewContainerRef = viewChild.required('container', { read: ViewContainerRef });
+
+  private readonly injector = inject(Injector);
+  navigate(page: string) {
+    this.viewContainerRef().length !== 0 && this.viewContainerRef().clear();
+    this.viewContainerRef().createEmbeddedView(this.templateRef(), { $implicit: page });
+    this.injector.get(ApplicationRef).tick();
+  }
+}
